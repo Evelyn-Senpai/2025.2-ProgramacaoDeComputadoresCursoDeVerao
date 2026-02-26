52bd48e847a8009f3e6e80da60fdfa92c71c8148
Evelyn-Senpai <128evelynaraujo@gmail.com>
2026-02-26 20:32:35 -0300

    Calculadora

--- 2026-02-26/Exemplo01.py
+++ 2026-02-26/Exemplo01.py
@@ -1,4 +1,5 @@
 print('--- Calculadora de soma ---')
 n1 = int(input('Digite o primeiro número: '))
 n2 = int(input('Digite o segundo número: '))
-print('A soma dos números é: ')
+soma = n1+n2
+print(f'A soma dos números é: {soma}')
